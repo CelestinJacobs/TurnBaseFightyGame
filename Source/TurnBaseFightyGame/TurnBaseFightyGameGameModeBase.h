45f738dd292294f3c4a47b5c9c573f42bf813c61
Célestin Jacobs <celestin.jacobs@student.nua.ac.uk>
2019-01-27 22:13:28 +0000

    Initial commit

---
+++ Source/TurnBaseFightyGame/TurnBaseFightyGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurnBaseFightyGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURNBASEFIGHTYGAME_API ATurnBaseFightyGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
