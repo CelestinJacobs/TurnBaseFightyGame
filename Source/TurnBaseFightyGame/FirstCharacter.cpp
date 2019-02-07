3a86366754430fa41f2a1267e91b35f770c30535
Célestin Jacobs <celestin.jacobs@student.nua.ac.uk>
2019-02-07 22:18:14 +0000

    CharacterPawnCLass
    started character pawn class. Attempted to give it a mesh and failed. Then gave it two ints for health and damage

---
+++ Source/TurnBaseFightyGame/FirstCharacter.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FirstCharacter.h"
+#include "Components/StaticMeshComponent.h"
+
+
+// Sets default values
+AFirstCharacter::AFirstCharacter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FirstCharacterAMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FirstCharacterAMesh"));
+	FirstCharacterAMesh->AttachTo(Root);
+	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	RootComponent = Root;
+	Health = 50;
+	Damage = 5;
+}
+
+// Called when the game starts or when spawned
+void AFirstCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFirstCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AFirstCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/TurnBaseFightyGame/FirstCharacter.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "FirstCharacter.generated.h"
+
+UCLASS()
+class TURNBASEFIGHTYGAME_API AFirstCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AFirstCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY()
+	USceneComponent* Root;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* FirstCharacterAMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	int Health;
+
+	UPROPERTY(VisibleAnywhere)
+	int Damage;
+
+
+	
+	
+};
